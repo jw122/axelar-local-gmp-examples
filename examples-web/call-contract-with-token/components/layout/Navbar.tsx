8e0d49fcc33da77bb3ad9e7d053e5b1284091fbb
jw122 <thejuliawu@gmail.com>
2022-07-09 02:25:15 -0400

    background image for sunflower land

--- examples-web/call-contract-with-token/components/layout/Navbar.tsx
+++ examples-web/call-contract-with-token/components/layout/Navbar.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from "react";
 
 export const Navbar = () => {
-    return (
-        <header className="h-16 bg-white shadow-sm">
-            <nav className="container flex items-center w-full h-full px-4 mx-auto">
-                <div>
-                    <img className="h-8" src="https://axelar.network/wp-content/uploads/2022/02/Axelar-Logo-Dark.svg" alt="axelar" />
-                </div>
-            </nav>
-        </header>
-    );
+  return (
+    <header className=" bg-white shadow-sm">
+      <nav className="container flex items-center w-full h-full px-2 mx-auto">
+        <div>
+          {/* <img
+            className="h-8"
+            src="https://axelar.network/wp-content/uploads/2022/02/Axelar-Logo-Dark.svg"
+            alt="axelar"
+          /> */}
+        </div>
+      </nav>
+    </header>
+  );
 };
---
+++ examples-web/call-contract-with-token/pages/onboarding.tsx
@@ -0,0 +1,182 @@
+import cn from "classnames";
+import type { NextPage } from "next";
+import React, { useCallback, useEffect, useState } from "react";
+import { wallet, isTestnet } from "../config/constants";
+import {
+  sendTokenToDestChain,
+  getBalance,
+  generateRecipientAddress,
+  truncatedAddress,
+} from "../utils";
+
+const Home: NextPage = () => {
+  const [recipientAddresses, setRecipientAddresses] = useState<string[]>([]);
+  const [balances, setBalances] = useState<string[]>([]);
+  const [senderBalance, setSenderBalance] = useState<string>();
+  const [txhash, setTxhash] = useState<string>();
+  const [loading, setLoading] = useState(false);
+
+  async function handleOnSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const amount = formData.get("amount") as string;
+    setLoading(true);
+    await sendTokenToDestChain(amount, recipientAddresses, setTxhash).finally(
+      () => {
+        setLoading(false);
+        handleRefreshSrcBalances();
+        handleRefreshDestBalances();
+      },
+    );
+  }
+
+  const handleRefreshDestBalances = useCallback(async () => {
+    const _balances = await getBalance(recipientAddresses, false);
+    setBalances(_balances);
+  }, [recipientAddresses]);
+
+  const handleRefreshSrcBalances = useCallback(async () => {
+    const [_balance] = await getBalance([wallet.address], true);
+    setSenderBalance(_balance);
+  }, []);
+
+  const handleOnGenerateRecipientAddress = () => {
+    const recipientAddress = generateRecipientAddress();
+    setRecipientAddresses([...recipientAddresses, recipientAddress]);
+  };
+
+  useEffect(() => {
+    handleRefreshSrcBalances();
+  }, [handleRefreshSrcBalances]);
+
+  return (
+    <div
+      style={{
+        backgroundImage: "url('/assets/sunflower-land.png')",
+        // height: "300px",
+        // minHeight: "10%",
+        height: "auto",
+        position: "fixed",
+        minWidth: "100%",
+        minHeight: "100%",
+      }}
+    >
+      <div>
+        <h1 className="text-4xl font-medium text-center">Sunflower Land </h1>
+        <h2 className="text-base text-center">
+          Instantly top up your Polygon wallet
+        </h2>
+
+        <div className="grid grid-cols-2 gap-20 mt-20 justify-items-center">
+          {/* ETHEREUM CARD */}
+          <div className="row-span-2 shadow-xl card w-96 bg-base-100">
+            {/* <figure
+              className="h-64 bg-center bg-no-repeat bg-cover image-full"
+              style={{ backgroundImage: "url('/assets/avalanche.gif')" }}
+            /> */}
+            <div className="card-body">
+              <h2 className="card-title">Ethereum (Token Sender)</h2>
+
+              <p>
+                Sender ({truncatedAddress(wallet.address)}) balance:{" "}
+                {senderBalance}
+              </p>
+              <p>Send USDC to the Polygon network in seconds</p>
+              <div className="justify-end mt-2 card-actions">
+                <form
+                  className="flex flex-col w-full"
+                  onSubmit={handleOnSubmit}
+                >
+                  <div className="flex">
+                    <input
+                      disabled={loading}
+                      required
+                      name="amount"
+                      type="number"
+                      placeholder="Enter amount to send"
+                      className="w-full max-w-xs input input-bordered"
+                    />
+                    <button
+                      className={cn("btn btn-primary ml-2", {
+                        loading,
+                        "opacity-30":
+                          loading || recipientAddresses.length === 0,
+                        "opacity-100":
+                          !loading && recipientAddresses.length > 0,
+                      })}
+                      type="submit"
+                    >
+                      Send
+                    </button>
+                  </div>
+                  {txhash && isTestnet && (
+                    <a
+                      href={`https://testnet.axelarscan.io/gmp/${txhash}`}
+                      className="link link-accent mt-2"
+                      target="blank"
+                    >
+                      Track at axelarscan
+                    </a>
+                  )}
+                  <span className="mt-2 font-bold">Recipients</span>
+                  {recipientAddresses.map((recipientAddress) => (
+                    <span key={recipientAddress} className="mt-1">
+                      {truncatedAddress(recipientAddress)}
+                    </span>
+                  ))}
+
+                  <button
+                    onClick={handleOnGenerateRecipientAddress}
+                    type="button"
+                    className={cn("btn btn-accent mt-2", {
+                      loading,
+                    })}
+                  >
+                    Add recipient
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          {/* Destination chain card */}
+          <div className="row-span-1 shadow-xl card w-96 bg-base-100">
+            {/* <figure
+              className="h-64 bg-center bg-no-repeat bg-cover image-full"
+              style={{ backgroundImage: "url('/assets/moonbeam.gif')" }}
+            /> */}
+            <div className="card-body">
+              <h2 className="card-title">Polygon (Token Receiver)</h2>
+              <div className="h-40">
+                <div className="w-full max-w-xs form-control">
+                  <div>
+                    {recipientAddresses.map((recipientAddress, i) => (
+                      <div
+                        key={recipientAddress}
+                        className="flex justify-between"
+                      >
+                        <span>{truncatedAddress(recipientAddress)}</span>
+                        <span className="font-bold">
+                          {balances[i] || `0.00`}
+                        </span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div
+                className="justify-center mt-5 card-actions"
+                onClick={handleRefreshDestBalances}
+              >
+                <button className="btn btn-primary">Refresh Balances</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
